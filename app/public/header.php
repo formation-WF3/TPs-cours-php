2407dd22d4805b12b9e2a7105432bd047e6a1b5f
gtaigniere <54893913+gtaigniere@users.noreply.github.com>
2023-06-16 17:09:43 +0200

    Ajout du fichier header.php

---
+++ app/public/header.php
@@ -0,0 +1,10 @@
+<header>
+    <a href="#"><h1>Titre du site</h1></a>
+    <nav>
+        <ul>
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Partie 1</a></li>
+            <li><a href="#">Partie 2</a></li>
+        </ul>
+    </nav>
+</header>
