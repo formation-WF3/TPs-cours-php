ae75b34c8970527522284f5908d14e1da42c6194
gtaigniere <54893913+gtaigniere@users.noreply.github.com>
2023-06-16 09:52:39 +0200

    Ajout d'un titre à l'exercice tp03

--- app/TP/tp03.php
+++ app/TP/tp03.php
@@ -3,9 +3,11 @@
     Ecrire une boucle for qui affiche tous les nombres impairs
     de 0 à 100 (inclu)
 */
+echo "<h2>Affichage de nombres impairs</h2>";
 
 $nombre = 100;
 
 for($i = 1; $i <= $nombre; $i += 2) {
     echo "$i ";
 }
+echo "<br>";
